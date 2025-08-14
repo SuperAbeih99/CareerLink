d907d90ddf47d450104289946b5c869df9ec54b7
SuperAbeih99 <abeihhamani24@gmail.com>
2025-08-14 18:49:09 +0000

    server health

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,5 @@
-console.log('[INDEX] api/index.js loaded at', new Date().toISOString());
-const serverless = require('serverless-http');
-const app = require('../app');
-console.log('[INDEX] app required OK');
+console.log("[INDEX] api/index.js loaded at", new Date().toISOString());
+const serverless = require("serverless-http");
+const app = require("../app");
+console.log("[INDEX] app required OK");
 module.exports = serverless(app);
